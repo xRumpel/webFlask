ae1dc0a2155b7b2eaa61520ed3fc7ee8b7251b65
xRumpel <164161359+xRumpel@users.noreply.github.com>
2024-10-11 18:05:40 +0700

    финал

--- app/models.py
+++ app/models.py
@@ -1,16 +1,18 @@
 from app import db
-from flask_login import UserMixin
 from app import login_manager
-class user(db.Model):
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(1000), unique=True, nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     clicks = db.Column(db.Integer, default=0)
+
     def __repr__(self):
         return f'User {self.username} - clicks: {self.clicks}'
 
 
 @login_manager.user_loader
 def load_user(user_id):
-    return user.query.get(int(user_id))
+    return User.query.get(int(user_id))
--- app/routes.py
+++ app/routes.py
@@ -2,16 +2,16 @@ from flask import render_template, request, redirect, url_for, flash
 from app import app, db, bcrypt
 from app.models import User
 from app.forms import LoginForm, RegistrationForm
-from flask_login import login_user, logout_user, login_required, current_user
+from flask_login import login_user, logout_user, current_user, login_required
 
 @app.route('/')
 @login_required
 def index():
     return render_template('index.html')
 
-@app.route('/registr', methods=['GET', 'POST'])
-def registration():
-    if curent_user.is_authenticated:
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -21,7 +21,7 @@ def registration():
         db.session.commit()
         flash('Вы успешно зарегистрировались!', 'success')
         return redirect(url_for('login'))
-    return render_template("register.html")
+    return render_template("register.html", form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -32,11 +32,11 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
+            login_user(user)
             return redirect(url_for('index'))
         else:
-            flash('Неверное имя пользователя или пароль', 'danger')
-    return render_template("login.html")
+            flash('Неверно введены данные аккаунта', 'danger')
+    return render_template("login.html", form=form)
 
 @app.route('/logout')
 def logout():
@@ -44,8 +44,8 @@ def logout():
     return redirect(url_for('login'))
 
 @app.route('/click')
-@app login_required()
+@login_required
 def click():
-    current_user.click +=1
+    current_user.clicks += 1
     db.session.commit()
     return redirect(url_for('index'))
